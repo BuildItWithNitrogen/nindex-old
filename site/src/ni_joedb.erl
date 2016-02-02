3efe5025b681f4d20e183c5e148ad0ae5c0b258a
Jesse Gumm <gumm@sigma-star.com>
2016-02-02 12:03:37 -0600

    5.4.1 - add API functions

--- site/src/ni_joedb.erl
+++ site/src/ni_joedb.erl
@@ -2,7 +2,12 @@
 
 %% Expected API exports
 -export([init_db/0, 
-         get_all/0
+         get_all/0,
+         new/3,
+         id/1, id/2,
+         topic/1, topic/2,
+         descriptor/1, descriptor/2,
+         url/1, url/2
         ]).
 
 %% --------------------------------------------- 
@@ -33,3 +38,37 @@ put_all(Data) ->
 init_db() -> 
     put_all(seed()).
 
+%% API functions
+
+new(Topic, Descriptor, Url) ->
+    [{topic,Topic},
+     {descriptor,Descriptor},
+     {url,Url}].
+
+id(Weblink) ->
+    proplists:get_value(id, Weblink).
+
+topic(Weblink) ->
+    proplists:get_value(topic, Weblink).
+
+descriptor(Weblink) ->
+    proplists:get_value(descriptor, Weblink).
+
+url(Weblink) ->
+    proplists:get_value(url, Weblink).
+
+set_value(Weblink, Key, Value) ->
+    Weblink2 = proplists:delete(Key, Weblink),
+    [{Key, Value} | Weblink2].
+
+id(Weblink, ID) ->
+    set_value(Weblink, id, ID).
+
+topic(Weblink, Topic) ->
+    set_value(Weblink, topic, Topic).
+
+descriptor(Weblink, Descriptor) ->
+    set_value(Weblink, descriptor, Descriptor).
+
+url(Weblink, Url) ->
+    set_value(Weblink, url, Url).
